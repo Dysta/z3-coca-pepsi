0e82f2ecf4f53a451690adcfd013d2162f35c045
Dysta <xav4098@gmail.com>
2019-10-31 18:05:23 +0100

    first commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <Graph.h>
+#include <Parsing.h>
+#include "Z3Tools.h"
+
+int main(int argc, char* argv[]) {
+    
+    Graph g = getGraphFromFile("test");
+
+
+    return 0;
+}
