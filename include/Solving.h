eeb4c81d8b27bf93bad5df9e82469fa9bda8aae0
Thim <brocard.thimotee@gmail.com>
2019-11-13 13:15:21 +0100

    correction d'un truc que j'ai cassé

--- include/Solving.h
+++ include/Solving.h
@@ -16,12 +16,11 @@
 #include "Graph.h"
 #include <z3.h>
 
-enum mode {INCREASING, DECREASING};
 
 /**
  * @brief choose the order mode to explore length
  */
-enum order {DECREASING, INCREASING};
+enum mode {INCREASING, DECREASING};
 
 /**
  * @brief Generates a formula consisting of a variable representing the fact that @p node of graph number @p number is at position @p position of an accepting path.
